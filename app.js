28db2da3e884ae1151b17e5d4046cdb64cd1df0a
Rebecca Herrera <rebeccadherrera@gmail.com>
2021-12-15 21:29:22 -0500

    added code to app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+//notice the lack of parenthesis around the 'profileDataArr' parameter? 
+const printProfileData = profileDataArr => {
+    //this...
+    for (let i=0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('===========');
+
+    //is the same as this...
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
